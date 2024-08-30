8b346d120e4ee07f915839788e6288293fde1a7d
Soheil <soheilzarepoor79@gmail.com>
2024-08-30 13:32:09 +0330

    fix: handle error on deezer failure

--- src/deezer/deezer.service.ts
+++ src/deezer/deezer.service.ts
@@ -9,25 +9,28 @@ export class DeezerService {
 
     async searchSong(query: string) {
         const cachedResponse = await this.redisService.get(query);
+
         if (!cachedResponse) {
-            const response = await fetch(CONSTS.searchUrl + query);
-            if (response.ok) {
-                const { data } = await response.json();
-                this.redisService.set({
-                    key: query,
-                    value: JSON.stringify(data),
-                });
-                return data;
+            try {
+                const response = await fetch(CONSTS.searchUrl + query);
+                if (response.ok) {
+                    const { data } = await response.json();
+                    this.redisService.set({
+                        key: query,
+                        value: JSON.stringify(data),
+                    });
+                    return data;
+                }
+                throw new ServiceUnavailableException();
+            } catch (error) {
+                throw new ServiceUnavailableException();
             }
-            throw new ServiceUnavailableException();
         }
 
         return JSON.parse(cachedResponse);
     }
 
     async findById(externalID: string) {
-        console.log("calling the deezer api...");
-
         const response = await fetch(CONSTS.findByIdUrl + externalID);
         if (response.ok) {
             const data = await response.json();
